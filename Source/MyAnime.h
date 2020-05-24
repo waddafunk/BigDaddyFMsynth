f895d3cafaebc8963cc92b9be433621230b836ab
Davide De Marco <daveguitar.dema@gmail.com>
2020-05-24 17:03:17 +0200

    my anime setters and getters

--- Source/MyAnime.h
+++ Source/MyAnime.h
@@ -40,7 +40,47 @@ public:
         // update their positions.
     }
 
+
     void setComponentz() {}
+
+
+
+//---------------Getters----------------
+
+    float getSaturation(){return saturation;}
+    float getOpacity (){return opacity;}
+    float getBrightness(){return brightness;}
+    float getContrast (){return contrast;}
+    float getAlpha (){return alpha;}
+    float getHue (){return hue;}
+    float getInterpolated (){return interpolated;}
+    float getOverlaid (){return overlaid;}
+    int getFramesPassed (){return framesPassed;}
+    Colour getBaseColour(){return baseColour;}
+    Colour getInterpolateColour(){return interpolateColour;}
+    Colour getOverlayColour(){return overlayColour;}
+
+
+//--------------Setters---------------
+    void setSaturation(float sat){this->saturation = sat;}
+    void setOpacity (float opacity){this->opacity = opacity;}
+    void setBrightness(float brightness){this->brightness = brightness;}
+    void setContrast (float contrast){this->contrast = contrast;}
+    void setAlpha (float alpha){this->alpha = alpha;}
+    void setHue (float hue){this->hue = hue;}
+    void setInterpolated(float inter){this->interpolated = interpolated;}
+    void setOverlaid (float overlaid){this->overlaid = overlaid;}
+    void setFramesPassed (int frames){this->framesPassed = frames;}
+    void setBaseColour (Colour baseColour){this->baseColour = baseColour;}
+    void setInterpolateColour (Colour interpolateColour){this->interpolateColour = interpolateColour;}
+    void setOverlayColour (Colour overlayColour){this->overlayColour = overlayColour;}
+
+
+
+
+private:
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MyAnime)
 
     float saturation = 1.0f;
     float opacity = 1.0f;
@@ -51,16 +91,8 @@ public:
     float interpolated = 0.0f;
     float overlaid = 0.0f;
     int framesPassed = 0;
-
-
     Colour baseColour;
     Colour interpolateColour;
     Colour overlayColour;
 
-
-    
-
-private:
-    //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MyAnime)
 };
